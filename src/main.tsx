2a1a967e42ea724cda123be71f1bd40e8131f06e
wdm-coding <wdmcoding0620@163.com>
2026-06-01 17:46:51 +0800

    style: 调整prettier配置并格式化代码
    1. 将printWidth从100调整为80
    2. 格式化Test页面和main.tsx的代码缩进与换行

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/index.css';
 import router from '@/router';
 import { RouterProvider } from 'react-router-dom';
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
